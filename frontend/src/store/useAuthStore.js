a71067f5e905a8fb76cee1c567080423a38adb8c
pierre-alexandre cochet <76907126+pierre205@users.noreply.github.com>
2025-03-14 17:23:36 +0100

    login page + profile page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -37,4 +37,28 @@ export const useAuthStore = create((set) => ({
             set({isSigningUp:false});
         }
     },
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser:null});
+            toast.success("Logout successful");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in logout", error);
+        }
+    },
+    login: async (data) => {
+        set({isLoggingIn:true});
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser:res.data});
+            toast.success("Login successful");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in login", error);
+        } finally {
+            set({isLoggingIn:false});
+        }
+    },
+    updateProfile: async (data) => {}
 }));
